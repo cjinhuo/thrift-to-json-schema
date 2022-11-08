24db733e3d8d90296ca286ada59ee9ec78b327c2
陈金伙 <chenjinhuo@bytedance.com>
2022-11-08 13:07:41 +0800

    ci(parser-view): add base url

--- app/parser-view/vite.config.ts
+++ app/parser-view/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/thrift-to-json-schema' : '',
   build: {
     outDir: '../../dist',
   },

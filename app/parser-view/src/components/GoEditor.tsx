2b7f534cff848edbb18318ef90f9c4c67872a739
陈金伙 <chenjinhuo@bytedance.com>
2022-11-08 15:55:24 +0800

    feat(parse-view): optimize input of structString

--- app/parser-view/src/components/GoEditor.tsx
+++ app/parser-view/src/components/GoEditor.tsx
@@ -16,18 +16,24 @@ export default function GoEditor() {
       goContent: content,
     })
   }
-  const structString = `
-  # @flag header
-  # @schema additionalProperties:true
-  struct EmptyStruct {
+  const structString = `  struct EmptyStruct {
     string field_string
     double field_double
+    i8 field_i8
+    i16 field_i16
     i32 field_i32
     i64 field_i64
     bool field_bool
     optional string field_optional_string
+    optional map<string, string> map_string_string
+    optional map<string, i32> map_string_number
+    PayloadStruct payload
+    map<string, PayloadStruct> map_nest_payload
   }
-  `
+  struct PayloadStruct {
+    string field_string
+    double field_double
+  }`
   setGoContent({
     goContent: structString,
   })

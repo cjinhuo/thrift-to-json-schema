a41d23a9f584d3edb50157f9d60a06906be140ff
陈金伙 <chenjinhuo@bytedance.com>
2022-11-10 10:42:24 +0800

    feat(thrift-jsma): modify the result from exclusiveMinimum to minimum for  int keyword

--- packages/thrift-jsma/src/constants.ts
+++ packages/thrift-jsma/src/constants.ts
@@ -12,16 +12,16 @@ export enum FlagTypes {
 
 export const NumericalKeywordMap = {
   [SyntaxType.I8Keyword]: {
-    exclusiveMinimum: 0,
-    exclusiveMaximum: 255,
+    minimum: 0,
+    maximum: 255,
   },
   [SyntaxType.I16Keyword]: {
-    exclusiveMinimum: -32768,
-    exclusiveMaximum: 32767,
+    minimum: -32768,
+    maximum: 32767,
   },
   [SyntaxType.I32Keyword]: {
-    exclusiveMinimum: -2147483648,
-    exclusiveMaximum: 2147483647,
+    minimum: -2147483648,
+    maximum: 2147483647,
   },
   [SyntaxType.I64Keyword]: {
     // so large that throw error if set forcedly
